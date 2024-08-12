bbe7171b79592fdb523f5c04b792dd79f60e76b5
예진 <50562634+YejinnKim@users.noreply.github.com>
2024-08-12 14:48:45 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 288 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+vector<int> v;
+vector<int> tmp;
+
+void mergeSort(int s, int e)
+{
+	if (e - s < 1) return;
+	
+	int m = s + (e - s) / 2;
+	mergeSort(s, m);
+	mergeSort(m + 1, e);
+
+	for (int i = s; i <= e; i++)
+		tmp[i] = v[i];
+	
+	int k = s;
+	int i = s;
+	int j = m + 1;
+	while (i <= m && j <= e) {
+		if (tmp[i] > tmp[j])
+			v[k++] = tmp[j++];
+		else
+			v[k++] = tmp[i++];
+	}
+	while (i <= m)
+		v[k++] = tmp[i++];
+	while (j <= e)
+		v[k++] = tmp[j++];
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	v.resize(n);
+	tmp.resize(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+
+	mergeSort(0, n - 1);
+
+	for (int i = 0; i < n; i++)
+		cout << v[i] << "\n";
+
+	return 0;
+}
