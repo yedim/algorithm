0aaac758e0eb9a776c0e2c22d2797f6d297173b8
예진 <50562634+YejinnKim@users.noreply.github.com>
2024-11-28 20:20:33 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -8,15 +8,15 @@ int main()
 	cin >> n >> k;
 
 	vector< vector<int> > dp(n + 1, vector<int>(n + 1, 0));
-	
+
 	for (int i = 0; i <= n; i++) {
 		dp[i][0] = 1;
 		dp[i][1] = i;
 		dp[i][i] = 1;
 	}
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++)
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j < i; j++)
 			dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
 	}
 
